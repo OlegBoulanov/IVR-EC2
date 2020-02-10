5e5944945011ce678aed910d4fcd1676b7fdb597
Oleg Boulanov <oleg.boulanov@gmail.com>
2020-02-10 07:57:54 -0500

    Attempt to fix policy

--- src/Ivr/IvrStack.cs
+++ src/Ivr/IvrStack.cs
@@ -46,8 +46,18 @@ namespace Ivr
             {
                 AssumedBy = new ServicePrincipal("ec2.amazonaws.com"),
             });
+            var policy = new Policy(this, "IvrStsAssumeRole", new PolicyProps{
+                Statements = new PolicyStatement[]{
+                    new PolicyStatement(new PolicyStatementProps{
+                        Effect = Effect.ALLOW,
+                        Actions = new string [] { "sts:AssumeRole", "sts:GetFederationToken" },
+                        Resources = new string [] { $"arn:aws:iam:{props.Env.Account}:role/{role.RoleName}" },
+                    }),
+                }
+            });
+            policy.AttachToRole(role);
+            //role.AttachInlinePolicy(policy);
             //role.AddManagedPolicy(new IvrPolicy(this, $"{id}_Policy"));
-            WriteLine($"Role: {role.RoleArn}");
 
             var securityGroup = new SecurityGroup(this, $"{id}_SG", new SecurityGroupProps
             {
@@ -71,18 +81,17 @@ namespace Ivr
                 userData.WithNewUser(props.UserName, props.UserPassword, props.UserGroups);
             }
             userData.WithNewFolder("C:\\ProgramData\\IvrStack\\Distrib\\")
+                .WithCommands($"new-item info.log -force -value \"User profile: $env:userprofile\"")    // C:\Users\Administrator
                 .WithDownloadAndInstall(
                     "https://aka.ms/vs/16/release/vc_redist.x86.exe",
                     "https://download.visualstudio.microsoft.com/download/pr/d12cc6fa-8717-4424-9cbf-d67ae2fb2575/b4fff475e67917918aa2814d6f673685/dotnet-runtime-3.0.1-win-x64.exe",
                     "https://github.com/OlegBoulanov/s3i/releases/download/v1.0.315/s3i.msi"
                 )
                 .WithEnvironmentVariables(new Dictionary<string, string>{
-                    {"s3i_args", "\"https://s3-ap-southeast-2.amazonaws.com/install.ap-southeast-2.elizacorp.com/CHAU-01/s3i.ini --verbose\"" },
+                    {"s3i_args", "\"https://github.com/OlegBoulanov/s3i/blob/develop/Examples/Config.ini --verbose\"" },
                 })
-                // need AWS credentials?
-                .WithFiles(new Dictionary<string, string>{
-                    { ".aws/credentials", $"[default]\ncredential_source = Ec2InstanceMetadata\nrole_arn = arn:aws:iam::{props.Env.Account}:role/{role.RoleName}" },
-                })
+                .WithEc2Credentials(props.UserName, props.Env.Account, role.RoleName)
+                .WithEc2Credentials("OtherUser", props.Env.Account, role.RoleName)
                 .WithCommands("s3i");
                 /*
                 */
