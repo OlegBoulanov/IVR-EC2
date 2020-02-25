cff4b30fd69bf10572c6d6db199708a9a7b8dba0
Boulanov, Oleg <oleg.boulanov@hms.com>
2020-02-25 15:43:50 -0500

    Unused method removed

--- src/IvrLib/PortExtensions.cs
+++ src/IvrLib/PortExtensions.cs
@@ -8,17 +8,6 @@ namespace IvrLib
 {
     public static class PortExtensions
     {
-        public static (string prot, int begin, int end) Split(this Port port)
-        {
-            // Port.ToString(): "UDP 2000-3000"
-            var prot_ports = port.ToString().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            if (2 == prot_ports.Length)
-            {
-                var ports = prot_ports[1].Split('-').Select(p => int.Parse(p)).ToArray();
-                return (prot_ports[0].ToUpper(), 0 < ports.Length ? ports[0] : 0, 1 < ports.Length ? ports[1] : 0);
-            }
-            throw new ArgumentException($"Can't split port '{port}'");
-        }
         public static Port Clone(this Port port, int startPort = 0, int endPort = 0)
         {
             var ps = PortSpec.Parse(port.ToString());
