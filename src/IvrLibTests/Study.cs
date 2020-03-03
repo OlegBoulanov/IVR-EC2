854ce33dc139751a506fd0e786e43ba445e6a1f6
Boulanov, Oleg <oleg.boulanov@hms.com>
2020-03-03 12:47:53 -0500

    InvalidFilePathChars() study

--- src/IvrLibTests/Study.cs
+++ src/IvrLibTests/Study.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -25,5 +26,14 @@ namespace IvrLibTests
             Assert.IsFalse(0 < list?.Count());
             Assert.IsTrue(0 == (list?.Count() ?? 0));
         }
+        [Test]
+        public void InvalidFilePathChars()
+        {
+            var invalidPathChars = "|\0\u0001\u0002\u0003\u0004\u0005\u0006\a\b\t\n\v\f\r\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f";
+            var invalidFileChars = "\"<>" + invalidPathChars + ":*?\\/";
+            Assert.AreEqual(invalidPathChars, new string(Path.GetInvalidPathChars()));
+            Assert.AreEqual(invalidFileChars, new string(Path.GetInvalidFileNameChars()));
+        }
+
     }
 }
