a6d80d22c2a88db477637ba53cf36ca08696b32b
Oleg Boulanov <oleg.boulanov@gmail.com>
2020-03-26 13:08:48 -0400

    Default VolumeSize = 60

--- src/IvrLib/Stack/IvrInstanceProps.cs
+++ src/IvrLib/Stack/IvrInstanceProps.cs
@@ -12,15 +12,21 @@ namespace IvrLib
         public InstanceClass InstanceClass { get; set; } = InstanceClass.BURSTABLE3;
         public InstanceSize InstanceSize { get; set; } = InstanceSize.LARGE;
         public WindowsVersion WindowsVersion { get; set; } = WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_FULL_BASE;
-        public double VolumeSize { get; set; } = 30;
+        public double VolumeSize { get; set; } = 60;
         public EbsDeviceVolumeType VolumeType { get; set; } = EbsDeviceVolumeType.STANDARD;
         public static InstanceProps InstanceProps(IVpc vpc, ISubnet subnet, IRole role, ISecurityGroup securityGroup, IvrInstanceProps props, string privateIpAddress = null)
         {
             var instanceProps = new InstanceProps
             {
+                Vpc = vpc,
+                VpcSubnets = new SubnetSelection
+                {
+                    //SubnetType = SubnetType.PUBLIC,
+                    //SubnetGroupName = subnetGroupName,
+                    Subnets = new ISubnet[] { subnet },
+                },
                 InstanceType = InstanceType.Of(props.InstanceClass, props.InstanceSize),
                 MachineImage = new WindowsImage(props.WindowsVersion),
-                Vpc = vpc,
                 BlockDevices = new BlockDevice[] {
                     new BlockDevice {
                         DeviceName = "/dev/sda1",
@@ -32,12 +38,6 @@ namespace IvrLib
                 },
                 Role = role,
                 SecurityGroup = securityGroup,
-                VpcSubnets = new SubnetSelection
-                {
-                    //SubnetType = SubnetType.PUBLIC,
-                    //SubnetGroupName = subnetGroupName,
-                    Subnets = new ISubnet[] { subnet },
-                },
             };        
             if(!string.IsNullOrWhiteSpace(privateIpAddress)){
                 instanceProps.PrivateIpAddress = privateIpAddress;
