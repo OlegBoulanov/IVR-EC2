5f86ad046b9c38775d9a9769c131cb288f3d2a99
Oleg Boulanov <oleg.boulanov@gmail.com>
2020-03-28 16:19:48 -0400

    Defaults changed to align with Amazon CIDR

--- src/IvrLib/Stack/IvrVpcProps.cs
+++ src/IvrLib/Stack/IvrVpcProps.cs
@@ -9,9 +9,9 @@ namespace IvrLib
     public class IvrVpcProps
     {
         public string VpcCidrAddr { get; set; } = "10.0.0.0";
-        public int VpcCidrMask { get; set; } = 16;  // class B
-        public int SubnetCidrMask { get; set; } = 24;   // class C 
+        public int VpcCidrMask { get; set; } = 24;  // 10.0.0.*
+        public int SubnetCidrMask { get; set; } = 27;   // .0, .32, .64, .96, ...
         public int MaxAzs { get; set; } = 2;
-        public const int MaxIpsPerSubnet = 251; // excluding 0, 1, 2, 3, 255
+        public const int MaxIpsPerSubnet = 251; // excluding 5 addresses: .0, .1, .2, .3,    .255
     }
 }
