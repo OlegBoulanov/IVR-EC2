43e0cd38d8704fae355e1d31eccfc157a6996126
Oleg Boulanov <oleg.boulanov@gmail.com>
2020-05-01 14:40:40 -0400

    RTP ports extended to 30000, per whitelisting page

--- src/IvrLib/SipProviders/PlivoZenTrunk.cs
+++ src/IvrLib/SipProviders/PlivoZenTrunk.cs
@@ -13,6 +13,7 @@ namespace IvrLib.SipProviders
     {
         // https://www.plivo.com/docs/sip-trunking/zentrunk-quickstart#creating-an-inbound-trunk
         // https://support.plivo.com/support/solutions/articles/17000012097-what-ip-addresses-do-i-need-to-whitelist-on-my-communications-infrastructure-for-zentrunk-sip-trunkin
+        // https://www.plivo.com/docs/sip-trunking#ip-address-whitelisting
         // there is more for other regions
         public PlivoZenTrunk() : base("Plivo", "Zentrunk SIP Trunking", new Dictionary<string, IEnumerable<SecurityGroupRule>> {
             // Here we need to define only Egress rules, Ingress will be derived from these during selection
@@ -25,11 +26,11 @@ namespace IvrLib.SipProviders
             }},
             { "us-west-1", new List<SecurityGroupRule> {
                 new EgressRule(Peer.Ipv4("13.52.9.0/25"), Port.Udp(5060), "SIP"),
-                new EgressRule(Peer.Ipv4("13.52.9.0/25"), Port.UdpRange(10000, 20000), "RTP"),
+                new EgressRule(Peer.Ipv4("13.52.9.0/25"), Port.UdpRange(10000, 30000), "RTP"),
             }},
             { "ap-southeast-2", new List<SecurityGroupRule> {
                 new EgressRule(Peer.Ipv4("13.238.202.192/26"), Port.Udp(5060), "SIP"),
-                new EgressRule(Peer.Ipv4("13.238.202.192/26"), Port.UdpRange(10000, 20000), "RTP"),
+                new EgressRule(Peer.Ipv4("13.238.202.192/26"), Port.UdpRange(10000, 30000), "RTP"),
             }},
         }) {}
     }
